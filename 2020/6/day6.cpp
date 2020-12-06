2f78ce24358fa78a5c402f3a9ece735c04d6f2ed
Christoph Rettinger <christoph.rettinger@fau.de>
2020-12-06 15:48:30 +0100

    added more fun with STL functions

--- 2020/6/day6.cpp
+++ 2020/6/day6.cpp
@@ -6,6 +6,7 @@
 #include <fstream>
 #include <sstream>
 #include <algorithm>
+#include <numeric>
 
 auto readFromFile(std::string fileName)
 {
@@ -41,13 +42,7 @@ auto readFromFile(std::string fileName)
 
 int getSumOfAnwers(const std::vector<std::string>& answers)
 {
-    int sumOfAnswers = 0;
-    for(const auto& group: answers)
-    {
-        int numGroupAnswers = group.size();
-        sumOfAnswers += numGroupAnswers;
-    }
-    return sumOfAnswers;
+    return std::accumulate(answers.begin(), answers.end(), 0, [](int a, std::string s){return a + s.size();});
 }
 
 std::vector<std::string> getUnionOfGroupAnswers(std::vector<std::vector<std::string>> & answers)
@@ -55,44 +50,78 @@ std::vector<std::string> getUnionOfGroupAnswers(std::vector<std::vector<std::str
     std::vector<std::string> combinedAnswerOfGroups;
     for( auto& group: answers)
     {
+        /*
+        // version 1
         std::string groupAnswer = "";
         for(auto answer: group)
         {
             std::string unionOfTwoAnswers = "";
             std::set_union(answer.begin(), answer.end(), groupAnswer.begin(), groupAnswer.end(), 
-                           std::inserter(unionOfTwoAnswers,unionOfTwoAnswers.begin()));
+                            std::inserter(unionOfTwoAnswers,unionOfTwoAnswers.begin()));
             groupAnswer = unionOfTwoAnswers;
         }
         combinedAnswerOfGroups.push_back(groupAnswer);
+        */
+        // version2
+        std::string allAnswers = std::accumulate(group.begin(), group.end(), std::string("")); // append all strings
+        std::sort(allAnswers.begin(), allAnswers.end());
+        auto last = std::unique(allAnswers.begin(), allAnswers.end()); //extract all duplicate char sequences..
+        allAnswers.erase(last, allAnswers.end()); // .. and remove them
+        combinedAnswerOfGroups.push_back(allAnswers);
     }
     return combinedAnswerOfGroups;
 }
 
 std::vector<std::string> getIntersectionOfGroupAnswers(std::vector<std::vector<std::string>> & answers)
 {
+    /*
     std::vector<std::string> combinedAnswerOfGroups;
     for( auto& group: answers)
     {
-        std::string groupAnswer = group[0];
-        for(auto answer: group)
-        {
-            std::string intersectionOfTwoAnswers = "";
-            std::set_intersection(answer.begin(), answer.end(), groupAnswer.begin(), groupAnswer.end(), 
-                                  std::inserter(intersectionOfTwoAnswers,intersectionOfTwoAnswers.begin()));
-            groupAnswer = intersectionOfTwoAnswers;
-        }
-        combinedAnswerOfGroups.push_back(groupAnswer);
+        // version 1
+        // std::string groupAnswer = group[0];
+        // for(auto answer: group)
+        // {
+        //     std::string intersectionOfTwoAnswers = "";
+        //     std::set_intersection(answer.begin(), answer.end(), groupAnswer.begin(), groupAnswer.end(), 
+        //                           std::inserter(intersectionOfTwoAnswers,intersectionOfTwoAnswers.begin()));
+        //     groupAnswer = intersectionOfTwoAnswers;
+        // }
+        // combinedAnswerOfGroups.push_back(groupAnswer);
+
+        // version 2
+        std::string intersectionOfAllAnswers = std::accumulate(std::next(group.begin()), group.end(), group[0],
+        [](std::string curS, std::string newS){
+            std::string intersection = "";
+            std::set_intersection(curS.begin(), curS.end(), newS.begin(), newS.end(),
+                                  std::inserter(intersection, intersection.begin()));
+            return intersection;
+            });
+        combinedAnswerOfGroups.push_back(intersectionOfAllAnswers);
+
     }
+    return combinedAnswerOfGroups;
+    */
+
+    // version 3
+    std::vector<std::string> combinedAnswerOfGroups;
+    std::transform(answers.begin(), answers.end(), std::back_inserter(combinedAnswerOfGroups),
+    [](std::vector<std::string>& group) -> std::string {
+        return std::accumulate(std::next(group.begin()), group.end(), group[0],
+        [](std::string curS, std::string newS){
+            std::string intersection = "";
+            std::set_intersection(curS.begin(), curS.end(), newS.begin(), newS.end(),
+                                  std::inserter(intersection, intersection.begin()));
+            return intersection;
+            });
+    } );
     return combinedAnswerOfGroups;
 }
 
 template<typename T>
 void printContent(const std::vector<T>& vec)
 {
-    for (const auto& entry : vec)
-    {
-        std::cout << entry << std::endl;
-    }
+    std::copy(vec.begin(), vec.end(), std::ostream_iterator<T>(std::cout, " "));
     std::cout << std::endl;
 }
 
