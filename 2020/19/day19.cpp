880071451f4af8d7587741592f2f14da8e8e1570
Christoph Rettinger <christoph.rettinger@fau.de>
2020-12-19 10:08:43 +0100

    Finished day 19.2

--- 2020/19/day19.cpp
+++ 2020/19/day19.cpp
@@ -121,9 +121,9 @@ FinalRule evaluateRule(Rule & rule, FinalRules& finalRules)
     return finalRule;
 }
 
-void evaluteRules(Rules& rules, FinalRules& finalRules)
+void evaluateRules(Rules& rules, FinalRules& finalRules, int terminationSize = 0)
 {
-    while(!rules.empty())
+    while(rules.size() > terminationSize)
     {
         for(auto ruleIt = rules.begin(); ruleIt != rules.end(); ++ruleIt)
         {
@@ -144,16 +144,72 @@ bool isMatchedByRule(std::string msg, FinalRule & rule)
     return std::find(rule.begin(), rule.end(), msg) != rule.end();
 }
 
+int numberOfMatchesFromFront(std::string msg, FinalRule & rule)
+{
+    int numMatches = 0;
+    for(const auto & r : rule)
+    {
+        int rLength = r.size();
+        std::string frontSubStr = msg.substr(0,rLength);
+        if(frontSubStr == r) return 1 + numberOfMatchesFromFront(msg.substr(rLength), rule);
+    }
+    return numMatches;
+}
+
+int numberOfMatchesFromBack(std::string msg, FinalRule & rule)
+{
+    for(const auto & r : rule)
+    {
+        int rLength = r.size();
+        int beginR = msg.size()-rLength;
+        if(beginR > 0)
+        {
+            std::string backSubStr = msg.substr(beginR);
+            if(backSubStr == r) return 1 + numberOfMatchesFromBack(msg.substr(0,beginR), rule);
+        }
+    }
+    return 0;
+}
+
+bool isMatchedByLoopedRules(std::string msg, FinalRule & rule42, FinalRule & rule31)
+{
+    // pattern: *42 *31, but number of matches of 31 has to be < number of matches of 42
+    // also, "fully matched" -> no unmatched remainder in message
+
+    int sizeRule42 = rule42[0].size();
+    int sizeRule31 = rule31[0].size();
+    int numMatches42 = numberOfMatchesFromFront(msg, rule42);
+    int numMatches31 = numberOfMatchesFromBack(msg, rule31);
+    int remainderMsgSize = msg.size() - numMatches31 * sizeRule31 - numMatches42 * sizeRule42;
+    
+    //std::cout << msg << " " << numMatches42 << " " << numMatches31 << " " << remainderMsgSize << std::endl;
+    return (remainderMsgSize == 0) && (numMatches42 > numMatches31)  && (numMatches31 > 0);
+}
+
 int main()
 {
     Rules rules;
     FinalRules finalRules;
     std::vector<std::string> messages;
     readFromFile("input.txt", rules, finalRules, messages);
-    evaluteRules(rules, finalRules);
-    FinalRule zeroRule = finalRules[0];
-    std::cout << "Final number of rules: " << zeroRule.size() << std::endl;;
-    
+
+    Rules rulesPart1 = rules;
+    FinalRules finalRulesPart1 = finalRules;
+    evaluateRules(rulesPart1, finalRulesPart1);
+    FinalRule zeroRule = finalRulesPart1[0];
+    std::cout << "Final number of rules: " << zeroRule.size() << std::endl;
     int result1 = std::count_if(messages.begin(), messages.end(), [&zeroRule](std::string s){return isMatchedByRule(s,zeroRule);});
     std::cout << "Result 1: " << result1 << std::endl;
+
+    Rules rulesPart2 = rules;
+    rulesPart2[8] = {{42},{42,8}};
+    rulesPart2[11] = {{42,31},{42,11,31}};
+    FinalRules finalRulesPart2 = finalRules;
+    evaluateRules(rulesPart2, finalRulesPart2, 3);
+    FinalRule rule42 = finalRulesPart1[42];
+    FinalRule rule31 = finalRulesPart1[31];
+    
+    std::cout << "Number of rules: " << rule42.size() << " " << rule31.size() << std::endl;
+    int result2 = std::count_if(messages.begin(), messages.end(), [&rule42,&rule31](std::string s){return isMatchedByLoopedRules(s,rule42,rule31);});
+    std::cout << "Result 2: " << result2 << std::endl;
 }
